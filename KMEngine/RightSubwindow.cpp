7dc1f48771b9afa490cdbe37c3fbfd7210fdf90b
SorinSeph <11281777+SorinSeph@users.noreply.github.com>
2025-01-30 22:52:36 +0200

    WIP spliting the right side of the edtor into 2 subwindows: outliner and properties

---
+++ KMEngine/RightSubwindow.cpp
@@ -0,0 +1,119 @@
+#include "RightSubwindow.h"
+
+void CRightSubwindow::CreateRightSubwindow(HWND& ParentHwnd)
+{
+	m_RightSubwindowHwnd = CreateWindow(
+        L"RightSubwindow",
+        NULL,
+        WS_CHILDWINDOW | WS_VISIBLE | WS_CLIPSIBLINGS,
+        0,
+        0,
+        0,
+        0,
+        ParentHwnd,
+        NULL,
+        (HINSTANCE)GetWindowLong(ParentHwnd, GWLP_HINSTANCE),
+        NULL
+    );
+}
+
+LRESULT CALLBACK CRightSubwindow::RightSubwindowProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    HDC         hdc;
+    PAINTSTRUCT ps;
+    RECT        rect;
+
+    switch (message)
+    {
+        case WM_CREATE:
+        {
+            SetWindowLong(hwnd, 0, 0);
+
+            GetClientRect(hwnd, &rect);
+
+            int width = rect.right - rect.left;
+            int height = rect.bottom - rect.top;
+
+            // Create the blue child window (top half)
+        //    OutlinerHwnd = CreateWindowEx(
+        //        0,                              // Optional window styles
+        //        L"Outliner",           // Window class
+        //        NULL,                           // No window text
+        //        WS_CHILD | WS_VISIBLE,          // Window style
+        //        0, 0,                           // Position
+        //        width, height / 2,              // Size
+        //        hwnd,                           // Parent window
+        //        NULL,                           // No menu
+        //        (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE),
+        //        NULL                            // Additional application data
+        //    );
+
+        //    if (OutlinerHwnd == NULL)
+        //    {
+        //        MessageBox(hwnd, L"Could not create outliner window.", L"Error", MB_OK | MB_ICONERROR);
+        //    }
+
+        //    // Create the red child window (bottom half)
+        //    PropertiesHwnd = CreateWindowEx(
+        //        0,                              // Optional window styles
+        //        L"Properties",            // Window class
+        //        NULL,                           // No window text
+        //        WS_CHILD | WS_VISIBLE,          // Window style
+        //        0, height / 2,                  // Position
+        //        width, height / 2,              // Size
+        //        hwnd,                           // Parent window
+        //        NULL,                           // No menu
+        //        (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE),
+        //        NULL                            // Additional application data
+        //    );
+
+        //    if (PropertiesHwnd == NULL)
+        //    {
+        //        MessageBox(hwnd, L"Could not create properties window.", L"Error", MB_OK | MB_ICONERROR);
+        //    }
+
+            return 0;
+        }
+
+        case WM_PAINT:
+        {
+            hdc = BeginPaint(hwnd, &ps);
+
+            GetClientRect(hwnd, &rect);
+            Rectangle(hdc, 0, 0, rect.right, rect.bottom);
+            HBRUSH Brush = CreateSolidBrush(RGB(40, 40, 140));
+            FillRect(hdc, &rect, Brush);
+
+            EndPaint(hwnd, &ps);
+            return 0;
+        }
+
+        //case WM_SIZE:
+        //{
+        //    int width = LOWORD(lParam);
+        //    int height = HIWORD(lParam);
+
+        //    // Resize the blue child window to top half
+        //    if (OutlinerProc)
+        //    {
+        //        SetWindowPos(OutlinerHwnd, NULL, 0, 0, width, height / 4, SWP_NOZORDER);
+        //    }
+
+        //    // Resize the red child window to bottom half
+        //    if (PropertiesProc)
+        //    {
+        //        SetWindowPos(PropertiesHwnd, NULL, 0, height / 4, width, (height * 3) / 4, SWP_NOZORDER);
+        //    }
+
+        //    return 0;
+        //}
+
+
+        case WM_DESTROY:
+        {
+            PostQuitMessage(0);
+            return 0;
+        }
+    }
+    return DefWindowProc(hwnd, message, wParam, lParam);
+}
