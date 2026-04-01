3ff590d71f3b8352bdaeeb6c8992059604e72a3f
Sorin Sefcenco <sefcencosorin@yahoo.com>
2026-04-01 10:06:30 +0300

    WIP GLTF animation by adding timers

--- KMEngine/UIMessageQueue.cpp
+++ KMEngine/UIMessageQueue.cpp
@@ -5,6 +5,9 @@
 #include "Modules/PhysicsModule.h"
 #include "Core/CoreClock.h"
 
+//for (int ChildIt = Anim.m_Nodes.size() - 1; ChildIt >= 0; --ChildIt)
+
+
 void CUIMessageQueue::RayPicking(int MouseX, int MouseY)
 {
     float m_InitialMousePosX = MouseX;
@@ -325,14 +328,6 @@ void CUIMessageQueue::ImportGLTF(std::string& FilePath, const std::string& FileC
 // Currently hardcoded for rectangle GLTF animation
 void CUIMessageQueue::TestPlayGLTFAnimation()
 {
-    CLogger& Logger = CLogger::GetLogger();
-    Logger.Log("Test lambda from UIMessageQueue.cpp, PlayGLTFAnimation()\n");
-    Logger.Log("Quat X = ", m_QuatX, ", Quat Y = ", m_QuatY, "Quat Z = ", m_QuatZ, ", Quat W = ", m_QuatW, "\n\n");
-}
-
-
-void CUIMessageQueue::PlayGLTFAnimation(std::string& FilePath, const std::string& FileContent)
-{
     CGraphicsModule* pGraphicsModule = static_cast<CGraphicsModule*>(m_pUIModule->m_pMediator->m_ModuleArray[1]);
     CTimerManager& TimerManager = CTimerManager::GetTimerManager();
     CScene& Scene = CScene::GetScene();
@@ -354,19 +349,42 @@ void CUIMessageQueue::PlayGLTFAnimation(std::string& FilePath, const std::string
         auto Anim = pGraphicsModule->m_EntityBuilder.GetGLTFAnimation();
         int KeyframeIt{ 0 };
 
+        auto AnimateLambda = [=]() mutable {
+            if (KeyframeIt < Anim.m_Samplers.at(4).m_Rotation.size())
+            {
+                // Local transforms
+                glm::quat BoneTopQuat{ Anim.m_Samplers.at(4).m_Rotation.at(KeyframeIt).w,
+                Anim.m_Samplers.at(4).m_Rotation.at(KeyframeIt).x,
+                Anim.m_Samplers.at(4).m_Rotation.at(KeyframeIt).y,
+                Anim.m_Samplers.at(4).m_Rotation.at(KeyframeIt).z,
+                };
+                glm::mat4 BoneTopTranslation = glm::translate(glm::mat4(1.0f), glm::vec3(0, 3, 0));
+                glm::mat4 BoneTopRotation = glm::mat4_cast(BoneTopQuat);
+                glm::mat4 LocalTransformBoneTop = BoneTopTranslation * BoneTopRotation;
+                glm::mat4 LocalTransformBoneRoot = glm::mat4(1.0f);
+
+                for (int ChildIt = Anim.m_Nodes.size() - 1; ChildIt >= 0; --ChildIt)
+                {
+
+                }
+
+                KeyframeIt++;
+            }
+        };
+
         auto PrintLambda = [=]() mutable {
             CLogger& Logger = CLogger::GetLogger();
             Logger.Log("Test lambda from UIMessageQueue.cpp\n");
 
             if (KeyframeIt < Anim.m_Samplers.at(4).m_Rotation.size())
             {
-                Logger.Log("Quat X = ", 
+                Logger.Log("Quat X = ",
                     Anim.m_Samplers.at(4).m_Rotation.at(KeyframeIt).x,
-                    ", Quat Y = ", 
+                    ", Quat Y = ",
                     Anim.m_Samplers.at(4).m_Rotation.at(KeyframeIt).y,
-                    "Quat Z = ", 
+                    "Quat Z = ",
                     Anim.m_Samplers.at(4).m_Rotation.at(KeyframeIt).z,
-                    ", Quat W = ", 
+                    ", Quat W = ",
                     Anim.m_Samplers.at(4).m_Rotation.at(KeyframeIt).w,
                     "\n\n");
 
@@ -415,5 +433,78 @@ void CUIMessageQueue::PlayGLTFAnimation(std::string& FilePath, const std::string
 
         //TimerManager.SetTimer3<CUIMessageQueue, void, &CUIMessageQueue::TestPlayGLTFAnimation>(this, 5.0f, 30.0f);
         TimerManager.SetTimerVariadicArgsLambda("AnimationTimer", 5.0f, 30.0f, PrintLambda);
+    }
+}
+
+
+void CUIMessageQueue::PlayGLTFAnimation(std::string& FilePath, const std::string& FileContent)
+{
+    //TestPlayGLTFAnimation();
+
+    CGraphicsModule* pGraphicsModule = static_cast<CGraphicsModule*>(m_pUIModule->m_pMediator->m_ModuleArray[1]);
+    CTimerManager& TimerManager = CTimerManager::GetTimerManager();
+    auto Anim = pGraphicsModule->m_EntityBuilder.GetGLTFAnimation();
+
+    CScene& Scene = CScene::GetScene();
+    auto& SceneEntityList = Scene.GetSceneList();
+    CGameEntity3D* pGameEntity{ nullptr };
+
+    for (auto& SceneEntityIt : SceneEntityList)
+    {
+        if (SceneEntityIt.m_GameEntityTag == "Knight")
+        {
+            pGameEntity = &SceneEntityIt;
+        }
+    }
+    auto ShaderProgram = pGameEntity->m_SceneGraph.m_pRootNode->m_tType.m_OpenGLResource.m_ShaderProgram;
+
+
+    for (uint16_t KeyframeIt = 0; KeyframeIt < Anim.m_AnimKeyframes.size(); KeyframeIt++)
+    {
+        auto AnimLambda = [=]() {
+            glm::quat BoneTopQuat{ Anim.m_Samplers.at(4).m_Rotation.at(KeyframeIt).w,
+                Anim.m_Samplers.at(4).m_Rotation.at(KeyframeIt).x,
+                Anim.m_Samplers.at(4).m_Rotation.at(KeyframeIt).y,
+                Anim.m_Samplers.at(4).m_Rotation.at(KeyframeIt).z,
+            };
+
+            // Local transforms
+            glm::mat4 BoneTopTranslation = glm::translate(glm::mat4(1.0f), glm::vec3(0, 3, 0));
+            glm::mat4 BoneTopRotation = glm::mat4_cast(BoneTopQuat);
+            glm::mat4 LocalTransformBoneTop = BoneTopTranslation * BoneTopRotation;
+            glm::mat4 LocalTransformBoneRoot = glm::mat4(1.0f);
+
+            // Global transforms
+            glm::mat4 globalTransformBoneRoot = LocalTransformBoneRoot;
+            glm::mat4 globalTransformBoneTop = globalTransformBoneRoot * LocalTransformBoneTop;
+
+            glm::mat4 Anim2InverseBindMatriceBoneTop{
+                1, -0, 0, -0,
+                -0, 1, -0, 0,
+                0, -0, 1, -0,
+                -0, 0, -0, 1
+            };
+
+            glm::mat4 Anim2InverseBindMatriceBoneRoot{
+                1, -0, 0, -0,
+                -0, 1, -0, 0,
+                0, -0, 1, -0,
+                -0, -3, -0, 1
+            };
+
+            glm::mat4 IdentityMatrix{ 1.f };
+
+            glm::mat4 BoneMatrixRoot = globalTransformBoneRoot * Anim2InverseBindMatriceBoneTop;
+            glm::mat4 BoneMatrixTop = globalTransformBoneTop * Anim2InverseBindMatriceBoneRoot;
+
+            //ourShader.setMat4("bones[0]", BoneMatrixRoot);
+            //ourShader.setMat4("bones[1]", BoneMatrixTop);
+            glUniformMatrix4fv(glGetUniformLocation(ShaderProgram, "bones[0]"), 1, GL_FALSE, &BoneMatrixRoot[0][0]);
+            glUniformMatrix4fv(glGetUniformLocation(ShaderProgram, "bones[1]"), 1, GL_FALSE, &BoneMatrixTop[0][0]);
+        };
+
+        float CurrentTime = TimerManager.m_pCoreClock->GetFDeltaTime();
+        float AnimTime = CurrentTime + Anim.m_AnimKeyframes.at(KeyframeIt);
+        TimerManager.SetTimerVariadicArgsLambda("AnimationTimer", AnimTime, 0.0f, AnimLambda);
     }
 }
